3cfddd45a7f749daa63e25e6ac38d20294e0c151
Felix Ekow Amoquandoh <105642899+felixEKow@users.noreply.github.com>
2023-02-16 10:22:22 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
 const cellElements = document.querySelectorAll(".game--cell");
+const restart = document.querySelector(".game--restart");
+const xclass = "-cross";
+const circleClass = "-circle";
+let circlesTurn;
 
-cellElements.forEach(cell => {
-    cell.addEventListener("click", handleClick, {once:true})
-})
+startGame();
+
+
+function startGame(){
+    circlesTurn = false;
+    cellElements.forEach(cell => {
+        cell.addEventListener("click", handleClick, {once:true})
+    })
+}
 
 function handleClick(e){
-    console.log("clicked");
+    const cell = e.target;
+    const currentClass = circlesTurn ? circleClass : xclass;
+    //place mark
+    placeMark(cell, currentClass)
+    //check for winner
+    //check for draw
+    //switch turns 
+    swapTurns()
 }
+
+function placeMark(cell, currentClass){
+  cell.classList.add(currentClass);
+}
+
+function swapTurns(){
+    circlesTurn = !circlesTurn;
+}
+
+restart.addEventListener("click", startGame);
